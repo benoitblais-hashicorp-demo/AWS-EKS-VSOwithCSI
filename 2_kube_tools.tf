d44bd81b9c3d56cab07a508548b6489221cd7665
Benoit Blais <benoit.blais@hashicorp.com>
2026-07-06 21:04:28 -0400

    fix(network): use numeric port 443 for aws-load-balancer-ssl-ports annotation to enforce TLS listener creation on AWS NLB

--- 2_kube_tools.tf
+++ 2_kube_tools.tf
@@ -62,7 +62,7 @@ controller:
     annotations:
       service.beta.kubernetes.io/aws-load-balancer-backend-protocol: tcp
       ${var.public_hosted_zone != "" ? format("service.beta.kubernetes.io/aws-load-balancer-ssl-cert: \"%s\"", try(aws_acm_certificate_validation.public[0].certificate_arn, "")) : "# TLS disabled"}
-      ${var.public_hosted_zone != "" ? "service.beta.kubernetes.io/aws-load-balancer-ssl-ports: \"https\"" : ""}
+      ${var.public_hosted_zone != "" ? "service.beta.kubernetes.io/aws-load-balancer-ssl-ports: \"443\"" : ""}
       service.beta.kubernetes.io/aws-load-balancer-cross-zone-load-balancing-enabled: true
       service.beta.kubernetes.io/aws-load-balancer-eip-allocations: ${aws_eip.nginx_ingress[0].id},${aws_eip.nginx_ingress[1].id},${aws_eip.nginx_ingress[2].id}
       service.beta.kubernetes.io/aws-load-balancer-scheme: internet-facing
