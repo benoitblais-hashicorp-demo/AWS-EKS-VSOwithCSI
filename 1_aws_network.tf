670f76d0169b507c46e9577d72d3e9fb6d12cafd
Benoit Blais <benoit.blais@hashicorp.com>
2026-07-07 21:26:10 -0400

    feat: standardise prefixing, network layout, and official AWS tagging

--- 1_aws_network.tf
+++ 1_aws_network.tf
@@ -1,15 +1,51 @@
 # Copyright IBM Corp. 2024, 2026
 
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "zone-type"
+    values = ["availability-zone"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+data "aws_ec2_instance_type_offerings" "supported" {
+  location_type = "availability-zone"
+
+  filter {
+    name   = "instance-type"
+    values = [var.instance_type]
+  }
+}
+
+locals {
+  candidate_azs = [
+    for az in data.aws_availability_zones.available.names : az
+    if contains(data.aws_ec2_instance_type_offerings.supported.locations, az)
+  ]
+
+  sorted_candidate_azs = sort(local.candidate_azs)
+
+  azs = slice(
+    local.sorted_candidate_azs,
+    0,
+    min(length(local.sorted_candidate_azs), 3)
+  )
+}
+
 module "vpc" {
-  source             = "terraform-aws-modules/vpc/aws"
-  version            = "6.6.0"
+  source             = "app.terraform.io/benoitblais-hashicorp/vpc/aws"
+  version            = "~> 0.0.1"
   name               = "${local.resources_prefix}-vpc"
-  cidr               = local.vpc_cidr
+  cidr               = var.vpc_cidr
   azs                = local.azs
   enable_nat_gateway = true
   single_nat_gateway = true
-  private_subnets    = [for idx, az in local.azs : cidrsubnet(local.vpc_cidr, 4, idx)]
-  public_subnets     = [for idx, az in local.azs : cidrsubnet(local.vpc_cidr, 8, idx + 48)]
+  private_subnets    = [for idx, az in local.azs : cidrsubnet(var.vpc_cidr, 4, idx)]
+  public_subnets     = [for idx, az in local.azs : cidrsubnet(var.vpc_cidr, 8, idx + 48)]
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
--- 1_locals.tf
+++ 1_locals.tf
@@ -1,64 +1,10 @@
 # Copyright IBM Corp. 2024, 2026
 
 locals {
-  customer_id      = "${random_string.identifier.result}-hashicat-inc"
   demo_name        = "secrets-operator"
-  demo_id          = "${local.customer_id}-${local.demo_name}"
-  global_id        = lower(substr(base64encode(local.demo_id), 0, 6))
-  resources_prefix = replace("hashicat-inc-${local.global_id}", "-$", "") # make sure prefixes don't end with a hyphen
-  vpc_cidr         = "10.0.0.0/16"
+  global_id        = random_string.identifier.result
+  resources_prefix = "${var.resources_prefix}-${local.global_id}"
 }
-
-data "aws_availability_zones" "available" {
-  filter {
-    name   = "zone-type"
-    values = ["availability-zone"]
-  }
-
-  filter {
-    name   = "state"
-    values = ["available"]
-  }
-}
-
-data "aws_ec2_instance_type_offerings" "supported" {
-  location_type = "availability-zone"
-
-  filter {
-    name   = "instance-type"
-    values = [var.instance_type]
-  }
-}
-
-locals {
-  candidate_azs = [
-    for az in data.aws_availability_zones.available.names : az
-    if contains(data.aws_ec2_instance_type_offerings.supported.locations, az)
-  ]
-
-  sorted_candidate_azs = sort(local.candidate_azs)
-
-  azs = slice(
-    local.sorted_candidate_azs,
-    0,
-    min(length(local.sorted_candidate_azs), 3)
-  )
-}
-
-#------------------------------------------------------------------------------------
-# Enable for debugging purposes
-#------------------------------------------------------------------------------------
-
-# resource "terraform_data" "availability_zones" {
-#   input = {
-#     availability_zones   = data.aws_availability_zones.available.names
-#     offerings            = data.aws_ec2_instance_type_offerings.supported.locations
-#     candidate_azs        = local.candidate_azs
-#     sorted_candidate_azs = local.sorted_candidate_azs
-#     azs                  = local.azs
-#     timestamp            = timestamp()
-#   }
-# }
 
 resource "random_string" "identifier" {
   length  = 4
