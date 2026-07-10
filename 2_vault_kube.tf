e08b59a6480cebc28ac42f4d9dc77cdbae7cb539
GitHub Action <notifications@github.com>
2026-07-10 12:55:39 +0000

    Automated Terraform Formatting

--- 2_vault_kube.tf
+++ 2_vault_kube.tf
@@ -24,13 +24,13 @@ resource "vault_auth_backend" "kube_auth" {
 
 # 2. Configure Vault to trust the EKS cluster using its endpoints and the TokenReview service account secret
 resource "vault_kubernetes_auth_backend_config" "kube_auth_cfg" {
-  count              = var.step_2 ? 1 : 0
-  depends_on         = [time_sleep.step_2]
-  namespace          = vault_namespace.namespace.path
-  backend            = vault_auth_backend.kube_auth[0].path
-  kubernetes_ca_cert = base64decode(module.eks.cluster_certificate_authority_data)
-  kubernetes_host    = module.eks.cluster_endpoint
-  token_reviewer_jwt = kubernetes_secret_v1.vault_token[0].data["token"]
+  count                  = var.step_2 ? 1 : 0
+  depends_on             = [time_sleep.step_2]
+  namespace              = vault_namespace.namespace.path
+  backend                = vault_auth_backend.kube_auth[0].path
+  kubernetes_ca_cert     = base64decode(module.eks.cluster_certificate_authority_data)
+  kubernetes_host        = module.eks.cluster_endpoint
+  token_reviewer_jwt     = kubernetes_secret_v1.vault_token[0].data["token"]
   disable_iss_validation = true
 }
 
